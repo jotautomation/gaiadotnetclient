8afc4e5acf2096437fc514a8f4204998089f0429
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2018-06-01 13:28:05 +0300

    Messy example code
    This needs to be cleaned some day
    
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- ClientExample/Program.cs
+++ ClientExample/Program.cs
@@ -15,7 +15,7 @@ namespace JOT.Client
 
             Thread.Sleep(5000);
 
-            var client = new JOTRestClient("http://localhost:1234/");
+            var client = new JOTRestClient("http://192.168.133.130:1234");
 
             client.FetchApplications();
 
@@ -23,18 +23,22 @@ namespace JOT.Client
             var i = client.Inputs;
             var a = client.StateApps;
 
+            /*
+             * // Here is example how to wait for application to go to certain state
             Task.Run(() =>
             {
                 a["MyStatefulApplication"].WaitState("FirstState");
                 Console.WriteLine("Hep!");
             }
             );
-
-            Console.WriteLine(a["MyStatefulApplication"].State);
+            */
+            Console.WriteLine(a["RobotToolLock"].State);
 
             Thread.Sleep(100);
 
-            a["MyStatefulApplication"].Actions["trigger-FirstState"]();
+            a["OduMacControl"].Actions["set-Work"]();
+
+            Console.WriteLine(a["RobotToolLock"].State);
 
             o["Output1"].SetOutput(true);
 
