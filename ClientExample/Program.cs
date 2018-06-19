9cac45b4930c1cfcfde36d8badc680fff5323873
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2018-06-19 13:23:31 +0300

    Sample code
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- ClientExample/Program.cs
+++ ClientExample/Program.cs
@@ -14,7 +14,7 @@ namespace JOT.Client
         static void Main(string[] args)
         {
 
-            var client = new JOTRestClient("http://192.168.133.130:1234");
+            var client = new JOTRestClient("http://172.23.225.118:1234");
 
             client.Populate();
 
@@ -25,13 +25,25 @@ namespace JOT.Client
 
             Console.WriteLine("State: " + client.State);
 
-            if(client.ReadyForTesting)
+            while (!client.TestBoxClosing)
             {
-                Console.WriteLine("Ready for testing!");
+                
+                Thread.Sleep(10);
             }
 
+            Console.WriteLine("Test box closing!");
+            // Now DUT(s) is locked and it is safe to attach battery connector, USB etc.
 
-            client.StateTriggers["Release"](new Dictionary<string, object> { { "testResult1", "pass" }, { "testResult2", "pass" }, { "testResult3", "fail" } });
+            while (!client.ReadyForTesting)
+            {                
+                Thread.Sleep(10);
+            }
+
+            //Now test box is fully closed and we are ready for actual testing.
+            Console.WriteLine("Ready for testing!");
+
+
+            client.StateTriggers["Release"](new Dictionary<string, object> { { "testResult1", "pass" }, { "testResult2", "pass" }, { "testResult3", "pass" } });
 
             // Change to FingerBase too. Note! Tool change can be defined also in G-code
             r.Actions["changeTo-FingerBase"]();
