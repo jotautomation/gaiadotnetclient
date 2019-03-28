958c458019c7e5d42d8cb829e979d8b8fee4eca5
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2019-03-28 09:50:43 +0200

    Release as nuget package
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- JOTGaiaClient/Properties/AssemblyInfo.cs
+++ JOTGaiaClient/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Client")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("JOTGaiaClient")]
+[assembly: AssemblyDescription("Client for JOT Automation Gaia machines")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JOT Automation")]
 [assembly: AssemblyProduct("Client")]
-[assembly: AssemblyCopyright("Copyright © JOT Automation 2016")]
+[assembly: AssemblyCopyright("Copyright © JOT Automation 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
